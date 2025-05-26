b3c6ea50d9b81763245a92f6415e262246fb3c2d
Ethan <ethanlau88@gmail.com>
2025-05-26 10:49:18 +0100

    init

---
+++ src/Game.cpp
@@ -0,0 +1,62 @@
+
+#include "Game.h"
+#include <iostream>
+
+Game::Game(sf::RenderWindow& game_window)
+  : window(game_window)
+{
+	srand(time(NULL));
+}
+
+Game::~Game()
+{
+
+}
+
+bool Game::init()
+{
+	if (!background_texture.loadFromFile("../Data/Images/background.png"))
+	{
+      std::cout << "background texture did not load \n";
+	}
+    background.setTexture(background_texture);
+	return true;
+}
+
+void Game::update(float dt)
+{
+
+}
+
+void Game::render()
+{
+  window.draw(background);
+}
+
+void Game::mouseClicked(sf::Event event)
+{
+	//get the click position
+	sf::Vector2i click = sf::Mouse::getPosition(window);
+
+	// check if in bounds of bird Sprite
+	if (collisionCheck(click, bird))
+	{
+
+	}
+}
+
+void Game::keyPressed(sf::Event event)
+{
+
+}
+
+
+bool Game::collisionCheck(sf::Vector2i click, sf::Sprite sprite)
+{
+	return false;
+}
+
+void Game::spawn()
+{
+
+}
